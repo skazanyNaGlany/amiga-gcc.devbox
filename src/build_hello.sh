359beacca1f7e80a9b38cea7a2b4365a0f956f14
Pawel <screamingbox@gmail.com>
2023-03-05 13:44:38 +0100

    Added two comments in the examples

--- src/build_hello.sh
+++ src/build_hello.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# -mcrt=nix13 means that we want to build for Kickstart 1.3
+
 rm a.out
 m68k-amigaos-gcc hello.c -mcrt=nix13
--- src/build_hello2.sh
+++ src/build_hello2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# -mcrt=nix13 means that we want to build for Kickstart 1.3
+
 rm a.out
 m68k-amigaos-gcc hello2.c -mcrt=nix13
